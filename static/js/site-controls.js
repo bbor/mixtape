41cbe3a2b443637859a7c8bdf3e5578e43ff2cd6
Robb Surridge <robb.surridge@autodesk.com>
2018-02-24 16:22:18 -0500

    Fix icons across platforms

--- static/js/site-controls.js
+++ static/js/site-controls.js
@@ -27,8 +27,8 @@ define(['jquery', 'hoverDelay'], function($) {
   site_controls.hide_control_panel_v = function () {
     var targetheight = (site_controls.breakpoint_level() == 1) ? '60px' : '40px';
     $('#control-panel').animate({'height':targetheight}, function() { $(this).removeAttr('style'); } );
-    $('#control-strip-h-find').removeAttr('style');
-    $('#control-strip-h-toc').removeAttr('style');
+    $('#h-find').removeAttr('style');
+    $('#h-toc').removeAttr('style');
   }
   site_controls.show_control_panel_v = function () {
     $('#control-panel').animate({'height':'100%'}, function() { $('input#find').focus(); });
@@ -36,16 +36,16 @@ define(['jquery', 'hoverDelay'], function($) {
   site_controls.show_find_control_v = function() {
     $('#control-find').show();
     $('#control-toc').hide();
-    $('#control-strip-h-toc').removeAttr('style');
-    $('#control-strip-h-find').css({'background-color':'orange'});
+    $('#h-toc').removeAttr('style');
+    $('#h-find').css({'background-color':'orange'});
     var mw = $('#controls').innerWidth() - 20;
     $('input#find').css({'width':mw});
   }
   site_controls.show_toc_control_v = function() {
     $('#control-toc').show();
     $('#control-find').hide();
-    $('#control-strip-h-find').removeAttr('style');
-    $('#control-strip-h-toc').css({'background-color':'orange'});
+    $('#h-find').removeAttr('style');
+    $('#h-toc').css({'background-color':'orange'});
   }
 
   // horizontal sliding panel, for tablet and desktop
@@ -144,7 +144,7 @@ define(['jquery', 'hoverDelay'], function($) {
       handlerIn:function() { if (site_controls.enable_wave_open) site_controls.show_toc_control_h(); }
     }).on('click', site_controls.show_toc_control_h);
 
-    $(document).on('click', function(e) {
+    $(document).on('click touchstart', function(e) {
       if($(e.target).closest('#control-panel, #control-strip-v').length)
           return;
       if (!site_controls.is_mobile())
